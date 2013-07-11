2db5116b042e81b6d9665aa5eee7b6a9edb96168
Adriano Godoy <godoy.ccp@gmail.com>
2013-07-10 21:06:21 -0300

    Projeto para base de hotsites e apps com login pelo facebook e upload de imagem por usuario

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookLoginPhotoUpload::Application.config.secret_token = '0c535b65125d8badc145db997c79297cc1b3814f76e9926fc61fcf0b36484af5e699f984f2a10b43142d92b70e20b284f90f98234e62694e5c08746870bac0ec'
